1d03d724b2d51b2f65531c3a0190f6b6ca6bec84
munees0210 <s.munees3107@gmail.com>
2024-10-25 12:00:04 +0530

    Update verifyUser.js

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -1,31 +1,3 @@
-// import jwt from 'jsonwebtoken';
-// import UserModel from '../models/User.js';
-
-// const verifyUser = async (req, res, next) => {
-//    try{
-//     const token = req.headers.authorization.split('')[1];
-//     if(!token) {
-//         return res.status(401).json({msg : "unauthorized"})
-//     }
-
-//     const decoded = jwt.verify(token, process.env.JWT_KEY);
-//     if(!decoded) {
-//         return res.status(401).json({msg : "invalid token"})
-//     }
-
-//     const user = await UserModel.findOne({_id: decoded.id}).select('-password');
-
-//     req.user = user;
-//     next()
-//    }catch(err) {
-//     console.log(err);
-//     return res.status(401).json({ msg: "Invalid token" });
-//    }
-
-// }
-
-// export default verifyUser;
-
 import jwt from 'jsonwebtoken';
 import UserModel from '../models/User.js';
 
